7f4115fde85d4036f126085bfa38819de70ac006
hayeong0206 <dbsgkdudchlr@ewhain.net>
2025-08-05 01:04:04 +0900

    feat(structure): Initial Setting with RDS MySQL

---
+++ src/main/java/com/example/wewha/WewhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wewha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WewhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WewhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wewha/WewhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wewha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WewhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
