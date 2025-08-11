ea70dc23c0229737376c68d1018c64fc7c5005e2
dustn926 <qkrdustn0926@naver.com>
2025-08-11 21:47:56 +0900

    댓글 좋아요

---
+++ src/main/java/com/example/wewha/comments/entity/CommentLike.java
@@ -0,0 +1,38 @@
+package com.example.wewha.comments.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "CommentLike", // 실제 테이블명이 comment_likes면 변경
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "comment_id"})
+)
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+}
