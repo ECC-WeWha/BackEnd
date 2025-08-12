d1ce02a281b5ec344ac60d620e88820d0481dfeb
dustn926 <qkrdustn0926@naver.com>
2025-08-12 21:57:34 +0900

    ee

--- src/main/java/com/example/wewha/comments/entity/Comment.java
+++ src/main/java/com/example/wewha/comments/entity/Comment.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+
 import java.time.Instant;
 
 import static jakarta.persistence.FetchType.LAZY;
