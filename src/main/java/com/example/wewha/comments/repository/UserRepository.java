6fa68457aade4f4f089e86159688b4d86309faa8
dustn926 <qkrdustn0926@naver.com>
2025-08-12 22:00:32 +0900

    댓글 좋아요 수정2

--- src/main/java/com/example/wewha/comments/repository/UserRepository.java
+++ src/main/java/com/example/wewha/comments/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.wewha.comments.repository;
 
-import com.example.wewha.comments.entity.User;
+import com.example.wewha.post.common.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {}
