bd9f05d479ef9e86f0d46c12348addf5a7978de1
dustn926 <qkrdustn0926@naver.com>
2025-08-12 22:00:32 +0900

    댓글 좋아요 수정2

--- src/main/java/com/example/wewha/comments/entity/CommentLike.java
+++ src/main/java/com/example/wewha/comments/entity/CommentLike.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import com.example.wewha.post.common.domain.User;
 
 import java.time.Instant;
 
