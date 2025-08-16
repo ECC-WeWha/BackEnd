547f9e940588250272ced8ab3b7f98227566b303
dustn926 <qkrdustn0926@naver.com>
2025-08-16 20:57:00 +0900

    댓글 수정 다시

--- src/main/java/com/example/wewha/common/entity/User.java
+++ src/main/java/com/example/wewha/common/entity/User.java
@@ -14,6 +14,8 @@ import java.sql.Timestamp;
 @Builder
 public class User {
 
+    // 또는 getId();
+    // 또는 getId();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
